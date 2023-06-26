99dc434ed91087b351c679957fb19439089a56c7
Juanjo Asensio García <asengar2009@gmail.com>
2023-06-26 15:18:13 +0200

    Start supporting i18n

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Component, Suspense } from "react";
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
 
-export default function Square() {
-    return <button className="square">X</button>;
+function Nav() {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" />
+                    {t('title')}
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default function App() {
+    return (
+        <Nav />
+    );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import './i18n'
 
 import App from "./App";
 
