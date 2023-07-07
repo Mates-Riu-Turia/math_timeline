f8babdb0c5cb16aa41aab40bb4db298f3b17de19
Juanjo Asensio García <asengar2009@gmail.com>
2023-07-07 15:01:29 +0200

    Add sample code for working with Apollo Client and MongoDB Atlas

---
+++ src/apollo.js
@@ -0,0 +1,39 @@
+import * as Realm from "realm-web";
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+// Connect to your MongoDB Realm app
+const app = new Realm.App("mamu-ehyjq");
+
+// Gets a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  // Guarantee that there's a logged in user with a valid access token
+  if (!app.currentUser) {
+    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
+    // access token.
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. Tokens must be refreshed after 
+    // 30 minutes. To guarantee that the token is valid, we refresh the user's access token.
+    await app.currentUser.refreshAccessToken();
+  }
+
+  return app.currentUser.accessToken;
+}
+
+// Configure the ApolloClient to connect to your app's GraphQL endpoint
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: `https://eu-west-2.aws.realm.mongodb.com/api/client/v2.0/app/mamu-ehyjq/graphql`,
+    // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
+    // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
+    // access token before sending the request.
+    fetch: async (uri, options) => {
+      const accessToken = await getValidAccessToken();
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,40 @@ import "./styles.css";
 import './i18n'
 
 import App from "./App";
+import client from "./apollo";
+import { ApolloProvider, gql } from "@apollo/client";
+
+client
+
+  .query({
+
+    query: gql`
+
+  query {
+    exposition {
+      _id
+      applications
+      curiosities
+      description
+      history
+      math_workshop
+      previous_knowledge
+      title
+      visual_workshop
+    }
+  }
+
+  `,
+
+  })
+
+  .then((result) => console.log(result));
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
